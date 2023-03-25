340d15ac9b2a4880bffff2eaae8abb668a39000c
Charmed <67402756+ever-nascent@users.noreply.github.com>
2023-03-25 16:02:44 -0700

    Update
    Switched to API rather than Scraping due to Rui being the TOS Police

--- scraper.js
+++ scraper.js
@@ -1,28 +1,52 @@
-const cheerio = require('cheerio');
 const fetch = require('node-superfetch');
-
 const pokedex = require('./pokedex.json');
-const base_url = 'https://pokemondb.net/pokedex';
 
-async function fetch_all_pokemon() {
+const fs = require('fs');
+
+const API_LINK = 'https://pokeapi.co/api/v2/pokemon';
+
+async function update_pokedex() {
 	try {
-		const all_pokemon_url = `${base_url}/all`;
+		const { body } = await fetch.get(API_LINK);
+		const to_add = await pokemon_to_add(body.count);
 
-		const { body } = await fetch.get(all_pokemon_url);
-		const $ = cheerio.load(body);
-
-		let total_pokemon = $('span.infocard-cell-data').eq(-1).text();
-		total_pokemon = parseInt(total_pokemon);
-		
-		const national_numbers_added = pokedex.map(pokedex.national_number);
-		let pokemon_numbers_array = Array.from({ length: total_pokemon }, (_, i) => i + 1);
-
-		const to_add = pokemon_numbers_array.filter(number => !national_numbers_added.includes(number));
-		console.log(national_numbers_added, pokemon_numbers_array, to_add);
+		const fetched_poke_data = await fetch_pokemon_data(to_add);
 
 	} catch (err) {
 		console.log(err);
 	}
 }
 
-fetch_all_pokemon();
+async function pokemon_to_add(total_pokemon) {
+	const list_of_pokemon_in_dex = pokedex.map(pokemon => pokemon.national_number);
+	let to_add = Array.from({ length: total_pokemon }, (_, i) => i + 1);
+
+	return to_add.filter(number => !list_of_pokemon_in_dex.includes(number));
+}
+
+async function fetch_pokemon_data(pokemon_to_fetch) {
+	for (i = 0; i < 1; i++) {
+		const { body } = await fetch.get(`${API_LINK}/${pokemon_to_fetch[i]}`);
+		console.log(body.sprites);
+		const parsed_pokedata = parse_pokedata(body);
+		pokedex.push(parsed_pokedata);
+	}
+
+	fs.writeFileSync('./pokedex.json', JSON.stringify(pokedex, null, 2), { encoding: 'utf8', flag: 'w', spaces: 2 });
+}
+
+function parse_pokedata(pokedata) {
+	return {
+		name: pokedata.name,
+		national_number: pokedata.id,
+		type: pokedata.types.map(types => types.type.name),
+		species: null,
+		height: pokedata.height,
+		weight: pokedata.weight,
+		abilities: pokedata.abilities.map(poke => poke.ability.name),
+		evolutions: null,
+		sprites: pokedata.sprites
+	}
+}
+
+update_pokedex();
